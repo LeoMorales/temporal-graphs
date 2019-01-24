c210797672d08b3795076156959b56e83233f99e
LeoMorales <leo.moralesr23@gmail.com>
2019-01-24 16:05:40 -0300

    pdf docs

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,8 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(1, os.path.abspath('..'))
 
+# from recommonmark.parser import CommonMarkParser
+
 
 # -- Project information -----------------------------------------------------
 
@@ -49,7 +51,7 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
+#source_suffix = ['.rst', '.md']
 source_suffix = '.rst'
 
 # The master toctree document.
@@ -65,7 +67,12 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+#    'build/*',
+]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
